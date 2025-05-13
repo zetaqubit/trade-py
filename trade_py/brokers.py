81237879ccbe1236e3acbd7079a55fd83b37b4ea
Zhao <x.zhao.y@gmail.com>
2025-05-13 15:53:23 -0700

    Factor out simulated_broker

--- trade_py/brokers.py
+++ trade_py/brokers.py
@@ -4,9 +4,8 @@ import pandas as pd
 
 from . import broker
 from . import configs
-from . import events
-from . import portfolio
 from .alpaca_broker import AlpacaBroker
+from .simulated_broker import SimulatedBroker
 
 
 def create(cfg: dict | str) -> broker.Broker:
@@ -16,73 +15,3 @@ def create(cfg: dict | str) -> broker.Broker:
     if not broker_cls or not issubclass(broker_cls, broker.Broker):
         raise NotImplementedError(f'Unknown {cfg.broker_type=}.')
     return broker_cls(cfg)
-
-
-class SimulatedBroker(broker.Broker):
-    def __init__(self, cfg: dict):
-        super().__init__(cfg)
-        print(cfg)
-        df = pd.read_csv(cfg.df_path)
-        df.timestamp = pd.to_datetime(df.timestamp)
-        print(f'Read {len(df)} rows.')
-        start, end = pd.to_datetime(cfg.start_date), pd.to_datetime(cfg.end_date)
-        df = df[(df.timestamp >= start) & (df.timestamp <= end)]
-        print(f'Kept {len(df)} rows from {cfg.start_date} to {cfg.end_date}.')
-        print(df)
-        self.df = df
-        self.current_i = -1
-        self.data = None
-
-        self.portfolio = portfolio.Portfolio()
-
-    def next(self):
-        if self.current_i >= len(self.df) - 1:
-            raise StopIteration()
-        self.current_i += 1
-        row = self.df.iloc[self.current_i]
-
-        def row_to_market_event(row):
-            return events.MarketEvent(
-                time=row.timestamp,
-                symbol=self.cfg.symbol,
-                ohlc=events.OHLC(o=row.open, h=row.high, l=row.low, c=row.close)
-            )
-        self.data = row_to_market_event(row)
-        self.portfolio.on_market_event(self.data)
-        return self.data
-
-
-    def process_order_event(self, order_event: events.OrderEvent):
-        o = order_event
-        price = self.data.ohlc.o
-        if o.quantity.shares is not None:
-            quantity = o.quantity.shares
-        elif o.quantity.value is not None:
-            quantity = o.quantity.value / price
-        fill_event = events.FillEvent(
-            time=o.time,
-            symbol=o.symbol,
-            quantity=quantity,
-            price=price,
-        )
-        self.portfolio.on_fill(fill_event)
-
-
-class DataFrameRowIterator:
-    def __init__(self, df, map_fn=None):
-        self._rows = df.itertuples(index=True)
-        self.map_fn = map_fn
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        item = next(self._rows)
-        if self.map_fn:
-            item = self.map_fn(item)
-        return item
-
-
-
-
-
---
+++ trade_py/simulated_broker.py
@@ -0,0 +1,68 @@
+from . import broker, events, portfolio
+
+import pandas as pd
+
+class SimulatedBroker(broker.Broker):
+    def __init__(self, cfg: dict):
+        super().__init__(cfg)
+        print(cfg)
+        df = pd.read_csv(cfg.df_path)
+        df.timestamp = pd.to_datetime(df.timestamp)
+        print(f'Read {len(df)} rows.')
+        start, end = pd.to_datetime(cfg.start_date), pd.to_datetime(cfg.end_date)
+        df = df[(df.timestamp >= start) & (df.timestamp <= end)]
+        print(f'Kept {len(df)} rows from {cfg.start_date} to {cfg.end_date}.')
+        print(df)
+        self.df = df
+        self.current_i = -1
+        self.data = None
+
+        self.portfolio = portfolio.Portfolio()
+
+    def next(self):
+        if self.current_i >= len(self.df) - 1:
+            raise StopIteration()
+        self.current_i += 1
+        row = self.df.iloc[self.current_i]
+
+        def row_to_market_event(row):
+            return events.MarketEvent(
+                time=row.timestamp,
+                symbol=self.cfg.symbol,
+                ohlc=events.OHLC(o=row.open, h=row.high, l=row.low, c=row.close)
+            )
+        self.data = row_to_market_event(row)
+        self.portfolio.on_market_event(self.data)
+        return self.data
+
+
+    def process_order_event(self, order_event: events.OrderEvent):
+        o = order_event
+        price = self.data.ohlc.o
+        if o.quantity.shares is not None:
+            quantity = o.quantity.shares
+        elif o.quantity.value is not None:
+            quantity = o.quantity.value / price
+        fill_event = events.FillEvent(
+            time=o.time,
+            symbol=o.symbol,
+            quantity=quantity,
+            price=price,
+        )
+        self.portfolio.on_fill(fill_event)
+
+
+class DataFrameRowIterator:
+    def __init__(self, df: pd.DataFrame, map_fn=None):
+        self._rows = df.itertuples(index=True)
+        self.map_fn = map_fn
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = next(self._rows)
+        if self.map_fn:
+            item = self.map_fn(item)
+        return item
+
